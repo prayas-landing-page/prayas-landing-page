e821ed95a366d71c3ca95ae522dce0dec0f72015
Fenil Gandhi <9128903+fenilgandhi@users.noreply.github.com>
2022-10-24 05:43:08 +0530

    Adding Transparent Navbar & Routing

--- src/components/landingpage/sectionRecognition.js
+++ src/components/landingpage/sectionRecognition.js
@@ -11,17 +11,17 @@ class SectionRecognition extends React.Component {
         return (
             <MDBContainer className='my-5'>
                 <MDBRow>
-                    <MDBCol md='4'>
-                        <MDBTypography tag='div' className='display-5 pb-3 mb-3'>
-                            Recognition
-                        </MDBTypography>
-                    </MDBCol>
                     <MDBCol md='8'>
                         <MDBTypography tag='div' className='display-7 pb-3 mb-3'>
                             Our small efforts have made waves as we have been
                             receiving motivational momentos from Shrimati
                             Anjaliben Vijaybhai Rupani spouses of ex chief ministers
                             of Gujarat and Kathiyawadi samajh.
+                        </MDBTypography>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                        <MDBTypography tag='div' className='display-5 pb-3 mb-3'>
+                            Recognition
                         </MDBTypography>
                     </MDBCol>
                 </MDBRow>
