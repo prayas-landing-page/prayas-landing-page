a4d049fc8cead9dc68207385047a3571776cbf4b
Fenil Gandhi <9128903+fenilgandhi@users.noreply.github.com>
2022-11-05 01:51:30 +0530

    Adding some events photos

--- src/components/landingpage/sectionHeader.js
+++ src/components/landingpage/sectionHeader.js
@@ -5,26 +5,28 @@ import {
 } from 'mdb-react-ui-kit';
 
 export default function SectionHeader() {
+    const images = [
+        require('../../media/header_01.jpeg'),
+        require('../../media/header_02.jpeg'),
+        // '../../media/header_01.jpeg',
+        // '../../media/header_02.jpeg',
+        // '../../media/header_01.jpeg',
+        // '../../media/header_02.jpeg'
+    ]
+    const carousel_images = (        
+        images.map((image_src, index) =>
+            <MDBCarouselItem
+                className='w-100 d-block'
+                itemId={index+1}
+                src={image_src}
+                key={index}
+                alt='Donation to needy image'
+            />
+        )
+    )
     return (
-        <MDBCarousel showControls showIndicators fade delay={2000}>
-            <MDBCarouselItem
-                className='w-100 d-block'
-                itemId={1}
-                src='https://mdbootstrap.com/img/new/slides/041.jpg'
-                alt='...'
-            />
-            <MDBCarouselItem
-                className='w-100 d-block'
-                itemId={2}
-                src='https://mdbootstrap.com/img/new/slides/042.jpg'
-                alt='...'
-            />
-            <MDBCarouselItem
-                className='w-100 d-block'
-                itemId={3}
-                src='https://mdbootstrap.com/img/new/slides/043.jpg'
-                alt='...'
-            />
+        <MDBCarousel fade interval={3000} pause={false}>
+            {carousel_images}
         </MDBCarousel>
     );
 }
