1c647f88fa88fa3f9f4081f1753000f470e685d9
Fenil Gandhi <9128903+fenilgandhi@users.noreply.github.com>
2023-02-27 22:24:59 +0530

    Adding performance optimization

--- src/components/landingpage/sectionDonors.js
+++ src/components/landingpage/sectionDonors.js
@@ -12,14 +12,14 @@ import {
 
 export default function SectionDonors() {
     const title = 'Our Donors';
-    const members = [
-        "Shree Pravin Shah (Lokhandwala)",
-        "Shree Bhavesh Hakani (Chhabildas developers Pvt Ltd.)",
-        "Nilesh Timbadia (Matushri Revakuvarba Mohanlal Timbadia)",
-        "Shree Manish Mehta (Mehta Tyres)",
-        "Shree Mehul Dholakia (DJ Toyota)",
+    const donors = [
+        "Shree Pravinbhai Shah (Lokhandwala)",
+        "Shree Bhaveshbhai Hakani (Chhabildas developers Pvt Ltd.)",
+        "Nileshbhai Timbadia (Matushri Revakuvarba Mohanlal Timbadia)",
+        "Shree Manishbhai Mehta (Mehta Tyres)",
+        "Shree Mehulbhai Dholakia (DJ Toyota)",
         "Shree Sthanakvasi Jain Sangh, Sector 22, Gandhinagar",
-        " Shree Vinubhai Kapasi (Top Impex Pvt. Ltd.)",
+        "Shree Vinubhai Kapasi (Top Impex Pvt. Ltd.)",
     ];
     return (
         <section className='py-5 navbar-bg'>
@@ -32,19 +32,16 @@ export default function SectionDonors() {
                 <MDBContainer>
                     <MDBRow>
                         {
-                            members.map((member, index) =>
-                                <MDBCol md='3' key={index} className="my-3">
+                            donors.map((donor, index) =>
+                                <MDBCol size="6" lg="3" md="4" key={index} className="my-3 shrink-50">
                                     <MDBCard shadow='0' className='shadow-4 bg-light bg-gradient h-100'>
                                         <MDBCardBody>
-                                            <div className="d-none d-md-block">
-
-                                                <span className="w-100 text-center text-danger py-4 d-grid">
-                                                    <MDBIcon fas icon="heart" size="3x" className="heart" style={{ 'animationDelay': Math.random().toFixed(1) + 's' }} />
-                                                    <MDBIcon fas icon="hand-holding" size="5x" className="" />
-                                                </span>
-                                            </div>
+                                            <span className="w-100 text-center text-danger py-4 d-grid">
+                                                <MDBIcon fas icon="heart" size="3x" className="heart" style={{ 'animationDelay': Math.random().toFixed(1) + 's' }} />
+                                                <MDBIcon fas icon="hand-holding" size="5x" className="" />
+                                            </span>
                                             <MDBCardText className="display-8">
-                                                {member}
+                                                {donor}
                                             </MDBCardText>
                                         </MDBCardBody>
                                     </MDBCard>
